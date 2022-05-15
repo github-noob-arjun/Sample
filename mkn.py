78eb9db7a4e505e2c5a12a55f6a53fd857d7f93e
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-05-15 16:13:26 +0530

    Update mkn.py

--- mkn.py
+++ mkn.py
@@ -18,6 +18,7 @@ async def info(motech, msg):
 
 @mkn.on_message(filters.private & filters.forwarded)                           
 async def info(motech, msg):
+    user = await motech.get_chat_member(update_channel, msg.chat.id)
     if msg.forward_from:
         text = "<u>𝐅𝐨𝐫𝐰𝐚𝐫𝐝 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧 👀</u> \n\n"
         if msg.forward_from["is_bot"]:
