6b21a17e72e9a1a710be41bad75719cddb02e223
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-05-15 15:18:57 +0530

    Create mkn.py

---
+++ mkn.py
@@ -0,0 +1,55 @@
+from pyrogram import Client, filters
+
+
+mkn = Client(
+      "id bot",
+      bot_token="5231845980:AAHLyejUGI9C6EanZK7id7QSI-iVIWEoJZ8",
+      api_id="6152295",
+      api_hash="2d291700c03d39c4fdb7092d1f34f07c",
+)
+
+CMD = [".", "/"]
+
+@mkn.on_message(filters.command(["start"], CMD))
+async def info(motech, msg):
+    await msg.reply_text(
+        text="hello"
+    )
+
+@mkn.on_message(filters.private & filters.forwarded)                           
+async def info(motech, msg):
+    if msg.forward_from:
+        text = "<u>𝐅𝐨𝐫𝐰𝐚𝐫𝐝 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧 👀</u> \n\n"
+        if msg.forward_from["is_bot"]:
+            text += "<u>🤖 𝐁𝐨𝐭 𝐈𝐧𝐟𝐨</u>"
+        else:
+            text += "<u>👤𝐔𝐬𝐞𝐫 𝐈𝐧𝐟𝐨</u>"
+        text += f'\n\n👨‍💼 𝐍𝐚𝐦𝐞 : {msg.forward_from["first_name"]}'
+        if msg.forward_from["username"]:
+            text += f'\n\n🔗 𝐔𝐬𝐞𝐫𝐍𝐚𝐦𝐞 : @{msg.forward_from["username"]} \n\n🆔 ID : <code>{msg.forward_from["id"]}</code>'
+        else:
+            text += f'\n\n🆔 𝐈𝐃 : `{msg.forward_from["id"]}`'
+        await msg.reply(text, quote=True)
+    else:
+        hidden = msg.forward_sender_name
+        if hidden:
+            await msg.reply(
+                f"❌️𝐄𝐫𝐫𝐨𝐫 <b><i>{hidden}</i></b> ❌️𝐄𝐫𝐫𝐨𝐫",
+                quote=True,
+            )
+        else:
+            text = f"<u>𝐅𝐨𝐫𝐰𝐚𝐫𝐝 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧 👀</u>.\n\n"
+            if msg.forward_from_chat["type"] == "channel":
+                text += "<u>📢 𝐂𝐡𝐚𝐧𝐧𝐞𝐥</u>"
+            if msg.forward_from_chat["type"] == "supergroup":
+                text += "<u>🗣️ 𝐆𝐫𝐨𝐮𝐩</u>"
+            text += f'\n\n📃 𝐍𝐚𝐦𝐞 {msg.forward_from_chat["title"]}'
+            if msg.forward_from_chat["username"]:
+                text += f'\n\n➡️ 𝐅𝐫𝐨𝐦 : @{msg.forward_from_chat["username"]}'
+                text += f'\n\n🆔 𝐈𝐃 : `{msg.forward_from_chat["id"]}`'
+            else:
+                text += f'\n\n🆔 𝐈𝐃 `{msg.forward_from_chat["id"]}`\n\n'
+            await msg.reply(text, quote=True)
+
+
+mkn.run()
