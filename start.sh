26bf52c2587190485c6984f176f0f3c4bb0c4a20
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-05-16 16:48:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/MrMKN/id.git /id     
+  git clone https://github.com/MrMKN/pyro-tester.git /pyro-tester     
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /id
+  git clone $UPSTREAM_REPO /pyro-tester
 fi
-cd /id
+cd /pyro-tester
 pip3 install -U -r requirements.txt
 echo "BOT IS STARTING⚡️⚡️⚡️"
 python3 mkn.py
