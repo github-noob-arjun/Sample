d89080e5b284a947073e2a5b70ada5d91c8391ed
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-07-18 13:06:52 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='new',
+      version='0.2.1',
+      description='Python module for interacting with Chatbase APIs',
+      url='https://github.com/MrMKN/pyro-tester',
+      author='Github',
+      author_email='None',
+      license='MIT Pro',
+      packages=['new'],      
+      zip_safe=False)
