94a8bccb7c027bed3848d65a316abfffeed3593b
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-07-18 14:27:39 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='new',
       version='0.2.1',
       description='Python module for interacting with Chatbase APIs',
-      url='https://github.com/MrMKN/pyro-tester',
+      url='https://github.com/github-noob-arjun/Sample',
       author='Github',
       author_email='None',
       license='MIT Pro',
