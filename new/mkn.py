47e20317e80664227d49c25c30ee903de40b2d81
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-07-18 22:27:03 +0530

    Update mkn.py

--- new/mkn.py
+++ new/mkn.py
@@ -1,14 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery 
+from Client.client import Clt2
 
-
-MKN = Client(
-      "protester",
-      bot_token="5589185991:AAGDjCCcp7IG1u_YQErui5yLwyfXizptXYI",
-      api_id="4738674",
-      api_hash="f2be74eaa9b1cb32498f45d04e4dbb54",
-)
-
-@MKN.on_message(filters.command("start"))
+@Clt2.on_message(filters.command("start"))
 async def start(client, message):
     await message.reply_text("Success Message 💥 1")
