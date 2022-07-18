0a6eb70826c556023236a0d8506cbfdb8515655a
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-07-18 23:20:31 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='new',
+setup(name='new1',
       version='0.2.1',
       description='Python module for interacting with Chatbase APIs',
       url='https://github.com/github-noob-arjun/Sample',
       author='Github',
       author_email='None',
       license='MIT Pro',
-      packages=['new'],      
+      packages=['new1'],      
       zip_safe=False)
