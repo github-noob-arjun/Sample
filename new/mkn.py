519a2395c8cecceec664608b77f8bbe0282d364b
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-07-18 23:11:34 +0530

    Update and rename new/mkn.py to sample1.py

--- new/mkn.py
+++
@@ -1,7 +0,0 @@
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery 
-from Client.client import Clt2
-
-@Clt2.on_message(filters.command("start"))
-async def start(client, message):
-    await message.reply_text("Success Message 💥 1")
---
+++ sample1.py
@@ -0,0 +1,13 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery 
+
+App1 = Client(
+      "app1",
+      bot_token="5589185991:AAGDjCCcp7IG1u_YQErui5yLwyfXizptXYI",
+      api_id="4738674",
+      api_hash="f2be74eaa9b1cb32498f45d04e4dbb54",
+)
+
+@App1.on_message(filters.command("start"))
+async def start(client, message):
+    await message.reply_text("Success Message 💥 1")
