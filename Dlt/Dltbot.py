2df57990af98d2b796c465b2e1731ded9e393d18
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-07-19 13:23:38 +0530

    Update Dltbot.py

--- Dlt/Dltbot.py
+++ Dlt/Dltbot.py
@@ -4,7 +4,7 @@ import asyncio
 
 DltBot = Client(
       "app1",
-      bot_token="5589185991:AAGDjCCcp7IG1u_YQErui5yLwyfXizptXYI",
+      bot_token="5592815069:AAFdddkNDJ4xKxkIOlEzOOjnwX8ieW7huiY",
       api_id="4738674",
       api_hash="f2be74eaa9b1cb32498f45d04e4dbb54",
 )
@@ -14,9 +14,9 @@ CHANNELS = [-1001527733655, -1001527733655]
 
 @DltBot.on_message(filters.command("start"))
 async def start(bot, message):
-    await message.reply("**✅ Bot working**")
+    await message.reply("**✅ Bot working\n\n⚠️This is private Bot. You can't Access**")
 
 @DltBot.on_message(filters.chat(CHANNELS) & media_filter)
 async def media(bot, message):
-    await asyncio.sleep(10) 
+    await asyncio.sleep(18000) 
     await message.delete()
