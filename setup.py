eb6a0d3f91bc5306de382b6486a652d646714c6e
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-07-19 13:17:19 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='new1',
-      version='0.2.1',
+setup(name='Dlt',
+      version='0.1.1',
       description='Python module for interacting with Chatbase APIs',
-      url='https://github.com/github-noob-arjun/Sample',
+      url='https://github.com/github-noob-arjun/channelMessaseDl',
       author='Github',
       author_email='None',
       license='MIT Pro',
-      packages=['new1'],      
+      packages=['Dlt'],      
       zip_safe=False)
